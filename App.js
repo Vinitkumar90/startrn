c391ae74afb8eec3b5737ee42338278f1301c1ed
vinit <vinitkumar93341@gmail.com>
2025-04-10 00:49:25 +0530

    added todo

--- App.js
+++ App.js
@@ -1,44 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,TouchableOpacity, TouchableHighlight } from 'react-native';
-import { useState } from 'react';
+import { useState } from "react";
+import { View,Text,TextInput,Button,FlatList } from "react-native";
 
 export default function App() {
-  const[count,setCount] = useState(0);
+ const[task, setTask] = useState('');
+ const[tasks, setTasks] = useState([]);
 
+ const addTask = () => {
+  if(task){
+    setTasks([...tasks, task])
+    setTask('');
+  }
+ }
 
   return (
-    <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-      {count === 5 && <Text style={{color:"red",marginBottom:10}}>upper limit hit!</Text>}
-      {count === -5 && <Text style={{color:"green",marginBottom:10}}>Lower limit hit!</Text>}
-
-
-      <Text>{count}</Text>
-      
-      <TouchableOpacity
-       onPress={() => {
-        if(count < 5)
-          setCount(count +1);
-       }}
-      style={{borderWidth:5,borderColor:'green',padding:10,margin:10}}>
-          <Text>Increase the count +</Text>
-      </TouchableOpacity>
-      <TouchableOpacity 
-         onPress={() =>{
-          if(count > -5){
-            setCount(count - 1)
-          }
-         }}
-      style={{borderWidth:5,borderColor:'red',padding:10,margin:10}} 
-     >
-          <Text>Decrease the count -</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={()=> setCount(0)}>
-        <Text>
-          Reset the counter
-        </Text>
-      </TouchableOpacity>
-    </View>
+   <View style={{flex:1, padding:20}}>
+    <TextInput
+      value={task}
+      onChangeText={setTask}
+      placeholder="Add a task"
+      style={{borderWidth:1, padding:5, marginBottom:10}}
+    />
+    <Button title="Add a task" onPress={addTask} />
+    <FlatList
+      data={tasks}
+      renderItem={({item}) => <Text>{item}</Text>}
+      keyExtractor={(item,index)=> index.toString()}
+    />
+   </View>
   );
 }
 
